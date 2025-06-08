9425e50e89d24f942a4a1549d36139f90bffe3d4
Waissi <adr.adm@protonmail.com>
2025-06-08 20:12:12 +0200

    updating url

--- src/connection.lua
+++ src/connection.lua
@@ -5,7 +5,7 @@ local enet = require "enet"
 local json = import "json"
 local host = enet.host_create()
 local https = require "https"
-local url = "https://bx6uvqcwjjdrkm7hgvk3643xhe0zsrkt.lambda-url.eu-central-1.on.aws/"
+local url = "https://qfigqmeles6mwpsyxoi2tbq52e0xwiul.lambda-url.eu-central-1.on.aws/"
 local userId = os.time()
 local server
 local serverChannels = {
